1f0b7d62b80af23d787db51c0491c57d2b3450f1
Shaman <wow2009zfx@gmail.com>
2014-12-27 16:39:21 +0800

    Add item ids

--- app/src/main/java/app/next/udacity/com/nextthing/NextThingAdapter.java
+++ app/src/main/java/app/next/udacity/com/nextthing/NextThingAdapter.java
@@ -4,11 +4,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by Shaman on 12/27/14.
  */
-public class NextThingAdapter extends BaseAdapter{
+public class NextThingAdapter extends BaseAdapter {
     @Override
     public int getCount() {
         return 5;
@@ -31,4 +36,24 @@ public class NextThingAdapter extends BaseAdapter{
     }
 
 
+    /**
+     * This class contains all butterknife-injected Views & Layouts from layout file 'item.xml'
+     * for easy to all layout elements.
+     *
+     * @author ButterKnifeZelezny, plugin for Android Studio by Inmite Developers (http://inmite.github.io)
+     */
+    static class ViewHolder {
+        @InjectView(R.id.like_count)
+        TextView mLikeCount;
+        @InjectView(R.id.title)
+        TextView mTitle;
+        @InjectView(R.id.description)
+        TextView mDescription;
+        @InjectView(R.id.image)
+        ImageView mImage;
+
+        ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
 }
