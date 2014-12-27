60ec0ebbe0916d0c2b5a5af143d66c23759e6419
Shaman <wow2009zfx@gmail.com>
2014-12-27 15:55:05 +0800

    Refactor fragment

---
+++ app/src/main/java/app/next/udacity/com/nextthing/NextThingFragment.java
@@ -0,0 +1,24 @@
+package app.next.udacity.com.nextthing;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Shaman on 12/27/14.
+ */
+public class NextThingFragment extends Fragment {
+
+    public NextThingFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
+
