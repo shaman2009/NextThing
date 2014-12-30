65c37adacc4ba954f0217a6f0d1ac7d25922e27e
Shaman <wow2009zfx@gmail.com>
2014-12-31 00:08:35 +0800

    Add add nextthing activity ui

---
+++ app/src/main/java/app/next/udacity/com/nextthing/AddNextThingActivity.java
@@ -0,0 +1,77 @@
+package app.next.udacity.com.nextthing;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.InjectView;
+
+
+public class AddNextThingActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_next_thing);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_next_thing, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        @InjectView(R.id.product_title)
+        EditText mProductTitle;
+        @InjectView(R.id.product_url)
+        EditText mProductUrl;
+        @InjectView(R.id.product_description)
+        EditText mProductDescription;
+        @InjectView(R.id.submit)
+        Button mSubmit;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_add_next_thing, container, false);
+            return rootView;
+        }
+    }
+}
--- app/src/main/java/app/next/udacity/com/nextthing/MainActivity.java
+++ app/src/main/java/app/next/udacity/com/nextthing/MainActivity.java
@@ -1,5 +1,6 @@
 package app.next.udacity.com.nextthing;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -50,12 +52,11 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add) {
+            Intent intent = new Intent(this, AddNextThingActivity.class);
+            this.startActivity(intent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
